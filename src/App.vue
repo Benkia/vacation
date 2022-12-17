d54e63996cb2eec51eaff9a176b9d4a9a3fb8078
Ben Kiani <ben.kiani@zadarastorage.com>
2022-12-17 19:42:20 +0000

    - Added dev contianer - Added logic to app - Support typscript - Created dockerfile

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,86 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+<script lang="ts">
+import { HebrewCalendar } from '@hebcal/core';
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+  data() {
+    return {
+      startDate: Date.now(),
+      endDate: '',
+      workdays: 0
+    }
+  },
+  methods: {
+    calcHolidays(start: Date, end: Date): number {
+
+      // Get holidays between start and end dates
+      var cal = HebrewCalendar.calendar({ start: start, end: end })
+      var numOfHolidays = 0
+      cal.forEach(element => {
+
+        console.log(element.getDesc())
+        console.log(element.getDate().greg())
+        console.log(this.isWOrkingDay(element.getDate().getDay()))
+        if (element.getDesc() === "Erev Pesach") {
+          numOfHolidays += 0.5
+        }
+      });
+
+      return numOfHolidays
+    },
+    async calcWorkingDays() {
+      // Validate input
+      if (!this.startDate || !this.endDate) {
+        alert('Please enter a start date and an end date ');
+        return;
+      }
+
+      // Parse dates
+      const start = new Date(this.startDate);
+      const end = new Date(this.endDate);
+      if (isNaN(start.getTime()) || isNaN(end.getTime())) {
+        alert('Invalid date format');
+        return;
+      }
+      var holidays = this.calcHolidays(start, end)
+      var currentDate = start
+      var numOfDays = 0
+
+      while (currentDate <= end) {
+
+        // Check if the current day is a weekday (Monday through Friday)
+        if (this.isWOrkingDay(currentDate.getDay())) {
+          numOfDays++;
+        }
+
+        // Increment the current date by one day
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+
+      this.workdays = numOfDays - holidays
+
+    },
+    isWOrkingDay(day: number): boolean { return day == 6 || day < 4 }
+  }
+
+
+})
 </script>
 
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <label>Start Date:</label>
+    <input type="Date" v-model="startDate"  />
+    <br />
+    <label>End Date:</label>
+    <input type="Date" v-model="endDate" />
+    <br />
+    <button @click="calcWorkingDays()">Check workdays</button>
+    <p v-if="workdays">There are {{ workdays }} working days between {{ startDate }} and {{ endDate
+    }}:</p>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
